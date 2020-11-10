5fb2fa6ff2d9d9eb3a87c0adce34c71240cc171c
TS22082 <ts22082@gmail.com>
2020-11-09 21:12:14 -0800

    set up password check

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,7 +1,15 @@
 const router = require("express").Router()
 
 router.get("/check/:password", (req, res) => {
-  res.send({ pw: req.params.password })
+  let securityLevel = 0
+  const pw = req.params.password
+  if (pw.match(/[a-z]+/)) securityLevel++
+  if (pw.match(/[A-Z]+/)) securityLevel++
+  if (pw.match(/[0-9]+/)) securityLevel++
+  if (pw.match(/[$@#&!]+/)) securityLevel++
+  if (pw.length > 8) securityLevel++
+
+  res.send({ pw: pw, passed: securityLevel, total: 5 })
 })
 
 module.exports = router
